61f9715c9b4efce311128dc1b955b66f847af536
Arthur <arthur.timbeau@outlook.fr>
2026-07-28 12:19:15 +0200

    retrait d'une colonne d'index des données et modfication de run_train_eval pour lancer les sess 1 et 2

--- training/run_train_eval.py
+++ training/run_train_eval.py
@@ -7,6 +7,10 @@ from training.src.optimize import optimize
 from training.src.evaluate import evaluator, evaluate_baselines
 from core.config import INTERMS, AE_NATURES, AE_DIMS, TRIALS_AE, TRIALS_GV, TRIALS_GM
 
+sess_AR = False
+sess_AT = False
+
+
 def main():
     os.makedirs("training/performance", exist_ok=True)
     df_full = load_clean_data()
@@ -29,63 +33,103 @@ def main():
         optimize_and_train_ae(df_train, residuelle=r, inter=i, latent_dim=dim, ae_nature=nature, n_trials=TRIALS_AE)
         print(f"   [CHRONO] AE {r}_{i}_D{nature}{dim} : {time.perf_counter()-t0:.1f}s")
 
-      # 2. PLAN D'EXPÉRIENCES (14 MODÈLES CIBLÉS)
+    # 2. PLAN D'EXPÉRIENCES (14 MODÈLES CIBLÉS)
+
+    if (sess_AT == sess_AR) : 
+        raise ValueError("Une campagne d'entraînement par session. Régler sess_AT et sess_AR sur deux valeurs différentes.")
+
 
     print("\n" + "="*80)
-    print(" PHASE 2 : PLAN D'EXPÉRIENCES (14 MODÈLES STRATÉGIQUES)")
+    print(" PHASE 2 : PLAN D'EXPÉRIENCES")
     print("="*80)
-    
-    test_models = [
-        # Q1: Comparaison de l'intégration BEM (0 vs 1 vs 2)
-        ('GM', '0', 'f', False, 'A'),
-        ('GM', '1', 'f', False, 'A'), # Point de Pivot
-        ('GM', '2', 'f', False, 'A'),
         
-        # Q2: Impact du format spatial (GM vs GV)
-        ('GV', '1', 'f', False, 'A'),
-        
-        # Q3: Grandeur cible (Forces vs Vitesses)
-        ('GM', '1', 'v', False, 'A'),
-        
-        # Q4: Fonction de perte (A vs B)
-        ('GM', '1', 'f', False, 'B'),
-        ('GM', '1', 'v', False, 'B'),
-        
-        # Q5: Apport de l'Auto-Encodeur (D0 vs DXY)
-        ('GM', '0', 'f', True,  'A'),
-        ('GM', '1', 'f', True,  'A'),
-        ('GM', '2', 'f', True,  'A'), # Servira de base pour comparer le 2+
-        ('GM', '1', 'v', True,  'A'),
-        ('GM', '2', 'v', True,  'A'), # Servira de base pour comparer le 2+
-        
-        # Q6: Apport du mode '+' (Projection dans l'espace latent)
-        ('GV', '2+', 'f', True,  'A'),
-        ('GV', '2+', 'v', True,  'A'),
-    ]
-    
-    for e, r, i, has_ae, opt in test_models:
-        
+    if sess_AT == True :
 
-        # On exclut les modes "1+" (qui n'ont pas de sens physique en fait)
-        if '1+' in r:
-            continue
+        """
+        Sur la session AT, on lance la session 1 : quel est le meilleur modèle sans BEM ?
+        """
+        
+        test_models = [
+            ## GM avec et sans encodeur avec loss B (2 modèles)
+            ('GM','0','f', False, 'B'),
+            ('GM','0','f', True, 'B'),
+
+            ## Modèles GV (8 modèles) 
             
-        ae_label = "DXY" if has_ae else "D0"
-        model_base_name = f"{e}_{r}_{i}_{ae_label}_{opt}"
-        
-        print(f"\n\n{'#'*80}")
-        print(f" PIPELINE : {model_base_name}")
-        print(f"{'#'*80}")
-        
-        n_trials_current = TRIALS_GV if e == 'GV' else TRIALS_GM
-        
-        t0 = time.perf_counter()
-        optimize(df_train, entree=e, residuelle=r, inter=i, has_ae=has_ae, option=opt, model_base_name=model_base_name, n_trials=n_trials_current)
-        print(f"   [CHRONO] optimize {model_base_name} : {time.perf_counter()-t0:.1f}s")
+                # Auto-encodeur ou non, loss A et f
+                ('GV', '0', 'f', False, 'A'),
+                ('GV', '0', 'f', True, 'A'),
 
-        t0 = time.perf_counter()
-        evaluator(df_train, df_test, entree=e, residuelle=r, inter=i, has_ae=has_ae, option=opt, baseline_scores=baseline_scores)
-        print(f"   [CHRONO] evaluator {model_base_name} : {time.perf_counter()-t0:.1f}s")
+                #Auto-encodeur ou non, loss B et f
+                ('GV', '0', 'f', False, 'B'),
+                ('GV', '0', 'f', True, 'B'),
+
+                # Auto-encodeur ou non, loss A et v
+                ('GV', '0', 'v', False, 'A'),
+                ('GV', '0', 'v', True, 'A'),
+
+                #Auto-encodeur ou non, loss B et v
+                ('GV', '0', 'v', False, 'B'),
+                ('GV', '0', 'v', True, 'B'),
+
+            ## GM_0_V_{avec ou sans AE}_B (2 modèles) 
+
+            ('GM', '0', 'v', False, 'B'),
+            ('GM', '0', 'v', True, 'B')
+
+        ]
+
+    if sess_AR == True : 
+        """
+          Sur la session AR, on lance la session 2 : quel est le meilleur modèle à BEM ?
+        """
+        
+        test_models = [
+            ## GV sans encodeur avec loss B (2 modèles)
+            ('GV','1','f', False, 'B'),
+            ('GV','1','v', False, 'B'),
+
+            ## GV avec encodeur et loss A (2 modèles)
+            ('GV', '1', 'f', True, 'A'),
+            ('GV', '1', 'v', True, 'A'),
+
+            ## {GV,GM}_1_{f,v}_AE_B (4 modèles)
+            # GV
+                ('GV','1','f', True, 'B'),
+                ('GV','1','v', True, 'B'),
+            # GM
+                ('GM', '1', 'f', True, 'B'),
+                ('GM', '1', 'v', True, 'B'),
+
+            ## GM_0_V_{avec ou sans AE}_B (2 modèles) 
+
+            ('GM', '0', 'v', False, 'B'),
+            ('GM', '0', 'v', True, 'B')
+        ]
+
+    for e, r, i, has_ae, opt in test_models:
+            
+
+            # On exclut les modes "1+" (qui n'ont pas de sens physique en fait)
+            if '1+' in r:
+                continue
+                
+            ae_label = "DXY" if has_ae else "D0"
+            model_base_name = f"{e}_{r}_{i}_{ae_label}_{opt}"
+            
+            print(f"\n\n{'#'*80}")
+            print(f" PIPELINE : {model_base_name}")
+            print(f"{'#'*80}")
+            
+            n_trials_current = TRIALS_GV if e == 'GV' else TRIALS_GM
+            
+            t0 = time.perf_counter()
+            optimize(df_train, entree=e, residuelle=r, inter=i, has_ae=has_ae, option=opt, model_base_name=model_base_name, n_trials=n_trials_current)
+            print(f"   [CHRONO] optimize {model_base_name} : {time.perf_counter()-t0:.1f}s")
+
+            t0 = time.perf_counter()
+            evaluator(df_train, df_test, entree=e, residuelle=r, inter=i, has_ae=has_ae, option=opt, baseline_scores=baseline_scores)
+            print(f"   [CHRONO] evaluator {model_base_name} : {time.perf_counter()-t0:.1f}s")
 
 if __name__ == "__main__":
     main()
